c73e471f8e05913a2b89225ec5c5ea5880b42aa4
Lydia Lei <llei194@student.fuhsd.org>
2023-12-27 23:50:56 -0800

    adjust model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,20 @@ import Island from '../models/Island';
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
-    
+    let screenScale = null; 
+    let screenPosition = [0, -6.5, -43]; 
+    let rotation = [0.1, 4.7, 0]; 
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9]; 
+    } else {
+      screenScale = [1, 1, 1]; 
+    }
+
+    return [screenScale, screenPosition, rotation];
   }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
@@ -26,7 +38,11 @@ const Home = () => {
           <spotLight />
           <hemisphereLight />
 
-          <Island />
+          <Island 
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
