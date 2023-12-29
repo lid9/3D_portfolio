069b43eaa4bd7a1a9b54eade88878166cef25710
Lydia Lei <llei194@student.fuhsd.org>
2023-12-28 22:07:44 -0800

    add emailjs

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,24 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const formRef = useRef(null); 
   const [form, setForm] = useState({ name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {}; 
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }; 
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    setIsLoading(true); 
+
+    emailjs.sendForm(
+      
+    )
+  }
+
   const handleFocus = () => {}; 
   const handleBlur = () => {}; 
 
@@ -15,6 +29,7 @@ const Contact = () => {
 
         <form
           className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
         >
           <label className="text-black-500 font-semibold">
             Name
