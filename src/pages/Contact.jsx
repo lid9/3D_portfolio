28707752612b55f4691a0db9798c682e8e6da2a9
Lydia Lei <llei194@student.fuhsd.org>
2023-12-28 22:34:51 -0800

    add email params

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,8 +15,19 @@ const Contact = () => {
     setIsLoading(true); 
 
     emailjs.sendForm(
-      
-    )
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Lydia",
+        from_email: form.email,
+        to_email: 'valyngo07@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false); 
+    })
   }
 
   const handleFocus = () => {}; 
